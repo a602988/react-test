d663bd18cf4abfd0da05685bc7032813579b8919
a602988 <a680603@gmail.com>
2022-04-15 14:34:03 +0800

    test

--- jsx/main.jsx
+++ jsx/main.jsx
@@ -1,85 +1,34 @@
-class Square extends React.Component {
-    render() {
-        return(
-            <button
-                className="square"
-                onClick={() => this.props.onClick()}
-            >
-                {this.props.value}
-            </button>
-        );
-    }
-}
+import Title from './title.jsx'
 
 
-class Board extends React.Component{
+class ShoopingList extends React.Component {
     constructor(props) {
+        console.log(props)
         super(props);
-        this.state = {
-            squares : Array(9).fill(null),
-        }
     }
-
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
-    }
-
-    renderSquare(i) {
-        return (
-            <Square
-                value={this.state.squares[i]}
-                onClick={()=> this.handleClick(i)}
-            />
-        );
-    }
-
     render(){
-        const status = "Next player: X";
         return(
-            <div>
-                <div className="status">{status}</div>
-                <div className="border-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="border-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="border-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+
+            <div className="shopping-list">
+
+                <h1>shopping list for {this.props.name}</h1>
+                <Title />
+
+                <ul>
+                    <li>Instragram</li>
+                    <li>whatsApp</li>
+                    <li>Oculas</li>
+                    <li>{this.props.phone}</li>
+                </ul>
             </div>
         );
     }
 }
 
-class Game extends React.Component{
-    render(){
-        return(
-            <div className="game">
-                <div className="game-border">
-                    <Board />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <div>{/* TODO */}</div>
-                </div>
-            </div>
-        );
-    }
-
-}
-// ================================================
+let myname = "jean";
+let myphone = "0988700588";
 
 ReactDOM.render(
-    <Game />,
-    document.getElementById("root")
-
+    <ShoopingList name={myname} phone={myphone} />,
+    document.getElementById('root')
 );
---
+++ jsx/title.jsx
@@ -0,0 +1,8 @@
+class Title extends React.Component {
+    render(){
+        return(<h1>This is Title</h1>)
+
+    }
+}
+
+export default Title;
