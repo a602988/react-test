612954d3995ec04b6171fe66395325bd867cadd0
a602988 <a680603@gmail.com>
2022-04-20 22:09:32 +0800

    test

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,14 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
     plugins: [
         new CleanWebpackPlugin(),
+        // new CopyWebpackPlugin([
+        //     //{ from: 'src/img', to: 'dist/img' },
+        // ]),
     ],
 });
